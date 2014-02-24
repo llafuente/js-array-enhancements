57f17ae0952afc430373eb7d21ccf3e98cf30a0a
Luis Lafuente <luis.lafuente@inetsys.es>
2014-02-24 17:57:35 +0100

    add oFilter

--- test/test.js
+++ test/test.js
@@ -274,8 +274,23 @@
 
             t.end();
         });
-
     });
+
+    test("array.oFilter", function (t) {
+
+        var input = [{a: 0, b: 1, c: 2}, {a: 1, b: 1, c: 1}, {a: 2, b: 2, c: 2}, {a: 0, b: 0, c: 2}],
+            sum = 0,
+            keys = [];
+
+        t.deepEqual(array.oFilter(input, {a:0}), [{a: 0, b: 1, c: 2}, {a: 0, b: 0, c: 2}]);
+        t.deepEqual(array.oFilter(input, {a:0, c:2}), [{a: 0, b: 1, c: 2}, {a: 0, b: 0, c: 2}]);
+        t.deepEqual(array.oFilter(input, {b:1}), [{a: 0, b: 1, c: 2}, {a: 1, b: 1, c: 1}]);
+        t.deepEqual(array.oFilter(input, {b:2}), [{a: 2, b: 2, c: 2}]);
+        t.end();
+    });
+
+
+    
 
 
 
